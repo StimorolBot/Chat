24b749354d5a868e31f6cbf145fa503233f53f70
bot <maximBotPy@gmail.com>
2024-07-23 00:21:44 +0300

    добавил сохранение кук и переадресацию после регистрации

--- front/src/page/Register.jsx
+++ front/src/page/Register.jsx
@@ -1,13 +1,16 @@
 import "./style/register.sass"
 
-import api from "../../config"
-import cookies from "../../config"
+import api from "../../config/api"
+import cookies from "../../config/cookies"
+
 import { useState } from "react"
 import { MainInput } from "../components/ui/input/MainInput"
 import { MainBtn } from "../components/ui/btn/MainBtn"
+import { useNavigate } from "react-router-dom"
 
 
 export function Register() {
+    const navigate = useNavigate()
     const [userData, setUserData] = useState([{
         "user_name": "","ttl": ""
     }])
@@ -15,17 +18,18 @@ export function Register() {
     const sendData = async (event) => {
         event.preventDefault()
         await api.post("/register", userData).then((response) => {
-            console.log(response)
             cookies.set(
-                "userId", response.data["user_id"],
-                { maxAge: response.data["ttl"] }
+                "user_cookie", response.data.data["user_id"],
+                {
+                    maxAge: response.data.data["ttl"],
+                    path: "/"
+                 }
             )
+            navigate("/")
         }).catch((error) => {
             console.log(error)
         })
     }
-
-
     return(
         <section className="register">
             <h2 className="register__title">
