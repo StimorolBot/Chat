88ddc0e6555f29a7ecf53d8e3dce112718b71822
bot <maximBotPy@gmail.com>
2024-07-23 00:22:35 +0300

    добавил роутер регистрации

---
+++ back/src/app/fast_register/api_v1/router.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, status, HTTPException
+
+from src.config import redis
+from src.core.response import Response
+from src.core.logger import user_logger
+from src.core.operations import generate_uuid, get_seconds
+
+from src.app.fast_register.schema import CreateUser
+
+
+register_router = APIRouter(prefix="/register", tags=["register"])
+
+
+# ограничить количество запросов
+@register_router.post("/")
+async def register_user(user_create: CreateUser):
+    user_id = generate_uuid()
+    ttl = get_seconds(minutes=user_create.ttl)
+    user_logger.info(f"Пользователь '{user_id}' создан")
+    await redis.set(name=user_id, value=user_id, ex=ttl)
+
+    return Response(
+        status_code=status.HTTP_201_CREATED,
+        detail="Пользователь успешно создан",
+        data={"user_id": user_id, "user_name": user_create.user_name}
+    )
