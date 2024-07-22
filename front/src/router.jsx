28302d016d90cf0c4807eb1a6ea42a1ca632b8b9
bot <maximBotPy@gmail.com>
2024-07-23 00:19:41 +0300

    добавил роутинг

---
+++ front/src/router.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom"
+
+import { Chat } from "./page/Chat"
+import { Register } from "./page/Register";
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Chat/>,
+    },
+    {
+      path: "/register",
+      element: <Register/>,
+    },
+    {
+      path: "*",
+      element: <h1>404</h1>,
+    },
+  ]);
+
+
+export default router
