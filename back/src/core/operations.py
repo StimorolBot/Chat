71c9b3a6f480dba035adce57559d8d012ec236e9
bot <maximBotPy@gmail.com>
2024-07-25 00:04:28 +0300

    добавил функции получения и установки данных в редис

--- back/src/core/operations.py
+++ back/src/core/operations.py
@@ -1,4 +1,6 @@
+import json
 from uuid import uuid4
+from src.config import redis
 from datetime import timedelta
 
 
@@ -8,3 +10,15 @@ def generate_uuid() -> str:
 
 def get_seconds(minutes: int) -> int:
     return int(timedelta(minutes=minutes).total_seconds())
+
+
+async def set_redis(name: str, data: dict, ttl: int):
+    data_str = json.dumps(data)
+    await redis.set(name=name, value=data_str, ex=ttl)
+
+
+async def get_redis(key: str) -> dict | None:
+    data_dict = await redis.get(key)
+    if not data_dict:
+        return None
+    return json.loads(data_dict)
