98246d90b3fae193fa610dad3c5621a2aaad8376
bot <maximBotPy@gmail.com>
2024-07-25 00:02:38 +0300

    внес незначительные изменения

--- front/config/api.js
+++ front/config/api.js
@@ -1,8 +1,9 @@
 import axios from "axios"
 
+axios.defaults.withCredentials = true
 const api = axios.create({baseURL: "http://127.0.0.1:8000",
     withCredentials: true,
-    someSite: "none"
+    crossDomain: true
 })
 
 export default api
--- front/config/cookies.js
+++ front/config/cookies.js
@@ -3,6 +3,7 @@ import Cookies from "universal-cookie"
 let cookies = new Cookies()
 cookies.partitioned = true
 cookies.httpOnly = true
+cookies.someSite="none"
 
 export default cookies
 
