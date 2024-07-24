b168ebcde61d52e2ea51c88252e5e11dcdb90fca
bot <maximBotPy@gmail.com>
2024-07-25 00:09:32 +0300

    теперь в редис добавляется словарь с uuid и списком из чатов

--- back/src/app/fast_register/api_v1/router.py
+++ back/src/app/fast_register/api_v1/router.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, status, HTTPException
 
-from src.config import redis
 from src.core.response import Response
 from src.core.logger import user_logger
+from src.core.operations import set_redis
 from src.core.operations import generate_uuid, get_seconds
 
 from src.app.fast_register.schema import CreateUser
@@ -17,7 +17,11 @@ async def register_user(user_create: CreateUser):
     user_id = generate_uuid()
     ttl = get_seconds(minutes=user_create.ttl)
     user_logger.info(f"Пользователь '{user_id}' создан")
-    await redis.set(name=user_id, value=user_id, ex=ttl)
+    await set_redis(
+        name=user_id,
+        data={"user_id": user_id, "chat": []},
+        ttl=ttl
+    )
 
     return Response(
         status_code=status.HTTP_201_CREATED,
