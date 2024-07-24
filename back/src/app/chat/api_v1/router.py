6e3f48672205dcd4d5c3effb5899cb7c603b1969
bot <maximBotPy@gmail.com>
2024-07-25 00:06:23 +0300

    добавил эндпоинт нахождения пользователя, исправил вебсокет

--- back/src/app/chat/api_v1/router.py
+++ back/src/app/chat/api_v1/router.py
@@ -5,33 +5,41 @@ from fastapi import (APIRouter, WebSocket, WebSocketDisconnect,
                      Cookie, status, HTTPException)
 
 from src.core.logger import ws_logger
+from src.core.response import Response
+from src.app.chat.schema import AddChat
+from src.core.operations import set_redis, get_redis
+
 from src.app.chat.connection_manager import manager
 
 chat_router = APIRouter(tags=["chat"])
 
 
-@chat_router.websocket("/{user_id}")
-async def websocket_endpoint(
-        websocket: WebSocket, user_id: UUID,
-        user_cookie: Annotated[str | None, Cookie()] = None):
+@chat_router.post("/")
+async def add_chat(search: AddChat) -> Response:
+    search_user = await get_redis(str(search.user_id))
+    if not search.cookies:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Пользователь не авторизирован")
+    elif not search_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Пользователь не найден")
+    search_user["chat"].append(search.cookies)
+    await set_redis(str(search.user_id), search_user, 1800)  # исправить ttl
+    return Response(status_code=status.HTTP_200_OK, detail="Чат найден")
 
+
+@chat_router.websocket("/{chat_id}")
+async def websocket_endpoint(
+        websocket: WebSocket, chat_id: UUID,
+        user_cookie: Annotated[str | None, Cookie()] = None):
     if not user_cookie:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Пользователь не авторизирован"
         )
-    elif UUID(user_cookie) != user_id:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Не далось найти страницу"
-        )
-
     ws_logger.info(msg=f"Подключился: {user_cookie}")
-    await manager.connect(websocket)
+    await manager.connect(user_cookie, websocket)
     try:
         while True:
             data = await websocket.receive_json()
-            await manager.send_personal_message(data["data"], data["id"], websocket)
+            await manager.send_personal_message(data["data"], chat_id)
     except WebSocketDisconnect:
-        manager.disconnect(websocket)
-
+        manager.disconnect(user_cookie)
