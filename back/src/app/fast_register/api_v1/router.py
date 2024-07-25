b08a86764eb02b562a8c4cc738ab14315f9694f7
bot <maximBotPy@gmail.com>
2024-07-25 23:55:32 +0300

    становил куки

--- back/src/app/fast_register/api_v1/router.py
+++ back/src/app/fast_register/api_v1/router.py
@@ -1,19 +1,18 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, Response
 
-from src.core.response import Response
 from src.core.logger import user_logger
 from src.core.operations import set_redis
+from src.core.response import Response as ResponseSchema
 from src.core.operations import generate_uuid, get_seconds
 
 from src.app.fast_register.schema import CreateUser
-
 
 register_router = APIRouter(prefix="/register", tags=["register"])
 
 
 # ограничить количество запросов
 @register_router.post("/")
-async def register_user(user_create: CreateUser):
+async def register_user(user_create: CreateUser, response: Response) -> ResponseSchema:
     user_id = generate_uuid()
     ttl = get_seconds(minutes=user_create.ttl)
     user_logger.info(f"Пользователь '{user_id}' создан")
@@ -22,8 +21,13 @@ async def register_user(user_create: CreateUser):
         data={"user_id": user_id, "chat": []},
         ttl=ttl
     )
-
-    return Response(
+    response.set_cookie(
+        key="user_cookie",
+        value=user_id,
+        samesite='none',
+        expires=ttl
+    )
+    return ResponseSchema(
         status_code=status.HTTP_201_CREATED,
         detail="Пользователь успешно создан",
         data={
