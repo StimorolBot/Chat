855499a50cea423048c0e4cf612a638ee783756b
bot <maximBotPy@gmail.com>
2024-07-30 00:02:47 +0300

    добавил отображение списка с чатами

--- front/src/components/section/ChatRoom.jsx
+++ front/src/components/section/ChatRoom.jsx
@@ -1,8 +1,29 @@
-export function ChatRoom() {
+import "./style/chat_room.sass"
+
+
+export function ChatRoom({ chatList }) {
+    const createUrl = async (chatId, event) => {
+        event.preventDefault()
+        history.pushState("", "", chatId)   
+    }
+
     return(
         <section className="chat-room">
             <h2 className="hidden__title">Чаты</h2>
-            <div className="wrapper">                    
+            <div className="wrapper">
+                <ul className="chat__container">
+                    { chatList.length != 0 ?
+                        chatList.map((chat, index) => {
+                            return(
+                                <li className="chat__item"
+                                    key={ index } onClick={(event) => createUrl(chat, event)}>
+                                    { chat }
+                                </li>
+                            )
+                        })
+                    : <h2>Список чатов пуст</h2>
+                    }
+                </ul>
             </div>
         </section>
     )
