0ee34e41c24a2ae3c3240f4766411c682fa522d4
bot <maximBotPy@gmail.com>
2024-07-30 00:34:06 +0300

    поменял список на словарь

--- back/src/app/chat/connection_manager.py
+++ back/src/app/chat/connection_manager.py
@@ -1,19 +1,23 @@
+from uuid import UUID
+from typing import Dict
 from fastapi import WebSocket
 
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        self.active_connections: Dict[UUID:WebSocket] = {}
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, user_id: UUID, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections.append(websocket)
+        self.active_connections[user_id] = websocket
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+    def disconnect(self, user_id: UUID):
+        del self.active_connections[user_id]
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+    async def send_personal_message(self, message: str, chat_id: UUID):
+        websocket = self.active_connections.get(chat_id)
+        if websocket:
+            await websocket.send_text(message)
 
     async def broadcast(self, message: str):
         for connection in self.active_connections:
