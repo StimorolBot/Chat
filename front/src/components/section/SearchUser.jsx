46e86bd97ae875385a064ac082287b26da99e311
bot <maximBotPy@gmail.com>
2024-07-30 00:01:41 +0300

    добавил обновление списка чатов при их добавлении

--- front/src/components/section/SearchUser.jsx
+++ front/src/components/section/SearchUser.jsx
@@ -1,11 +1,12 @@
 import "./style/search_user.sass"
 
-import api from "../../../config/api"
-import { SearchInput } from "../ui/input/SearchInput"
 import { useRef } from "react"
+import api from "../../../config/api"
+import { SearchBtn } from "../ui/btn/SearchBtn"
+import { SearchInput } from "../ui/input/SearchInput"
 
 
-export function SearchUser() {
+export function SearchUser({userInfo, setUserInfo }) {
     let searchRef = useRef("")
 
     const search = async (event) => {
@@ -13,7 +14,10 @@ export function SearchUser() {
 
         await api.post("/", {"user_id": searchRef.current.value}).
             then(( response )=>{
-                console.log(response.data)
+                setUserInfo({
+                    ...userInfo,
+                    chatList: [...userInfo["chatList"], response.data.data["chat_id"]]
+                })
             }).
             catch((error) => {
                 console.log(error)
@@ -26,9 +30,7 @@ export function SearchUser() {
             <h2 className="hidden__title">Поиск пользователей</h2>
             <form className="search-user-form" action="/" method="POST">
                 <SearchInput innerRef={ searchRef }/>
-                <button className="search-btn" onClick={(event) => search(event)}>
-                    <img className="" src="/public/icon/search.svg" alt="Поиск" />
-                </button>
+                <SearchBtn search={ search } />
             </form>
         </section>
     )
