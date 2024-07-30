9636feaf9c6c067329b7d6ce1f8437706fc23bbb
bot <maximBotPy@gmail.com>
2024-07-30 23:29:03 +0300

    добавил id пользователя

--- front/src/components/section/About.jsx
+++ front/src/components/section/About.jsx
@@ -1,7 +1,14 @@
-export function About() {
+import "./style/about.sass"
+
+
+export function About({userId}) {
+    
     return(
         <section className="about">
             <h2 className="hidden__title">Об аккаунте</h2>
+            <div className="wrapper">
+                <h3>#{userId}</h3>
+            </div>
         </section>
     )
 }
