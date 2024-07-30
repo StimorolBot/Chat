66586c0b15b38d02627b8e67937a9433375b89c9
bot <maximBotPy@gmail.com>
2024-07-30 23:25:07 +0300

    подключил кэш

--- back/src/main.py
+++ back/src/main.py
@@ -1,9 +1,22 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.docs import get_redoc_html, get_swagger_ui_html, get_swagger_ui_oauth2_redirect_html
 
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+
+from src.config import redis
 from src.app.chat.api_v1.router import chat_router
 from src.app.fast_register.api_v1.router import register_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+    yield
+
 
 app = FastAPI(title="chat", docs_url=None, redoc_url=None)
 
