639cfa3bf4a332b081d5523397fc0b19f3151dc3
bot <maximBotPy@gmail.com>
2024-08-05 22:50:05 +0300

    добавил имена классов

--- front/src/components/section/ChatRoom.jsx
+++ front/src/components/section/ChatRoom.jsx
@@ -1,27 +1,30 @@
 import "./style/chat_room.sass"
 
 
-export function ChatRoom({ chatList }) {
-    const createUrl = async (chatId, event) => {
-        event.preventDefault()
-        history.pushState("", "", chatId)   
+export function ChatRoom({ chatList, setUserIdUrl }) {
+    
+    const createUrl = async (chatId) => {
+        history.pushState("", "", chatId)
+        setUserIdUrl(chatId)
     }
 
     return(
         <section className="chat-room">
             <h2 className="hidden__title">Чаты</h2>
-            <div className="wrapper">
-                <ul className="chat__container">
+            <div className="wrapper wrapper-chat-room">
+                <ul className="chat__container chat__container_none">
                     { chatList.length != 0 ?
                         chatList.map((chat, index) => {
                             return(
                                 <li className="chat__item"
-                                    key={ index } onClick={(event) => createUrl(chat, event)}>
+                                    key={ index } onClick={(event) => createUrl(chat)}>
                                     { chat }
                                 </li>
                             )
                         })
-                    : <h2>Список чатов пуст</h2>
+                    : <h2 className="chat__none">
+                        Список чатов пуст
+                    </h2>
                     }
                 </ul>
             </div>
