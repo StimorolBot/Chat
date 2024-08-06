3b17a7a1f97e3e2db4cae5ad1766636fcb7d828c
bot <maximBotPy@gmail.com>
2024-08-06 22:22:11 +0300

    добавил компонент для поиска

---
+++ front/src/components/ui/btn/SearchBtn.jsx
@@ -0,0 +1,9 @@
+import "./style/search_btn.sass"
+
+export function SearchBtn ({ search }) {
+    return(
+        <button className="search-btn" onClick={(event) => search(event)}>
+            <img className="search-btn__img" src="/public/icon/search.svg" alt="Поиск" />
+        </button>
+    )
+}
