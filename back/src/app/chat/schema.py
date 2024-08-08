9756bb77489e5bf9f18e8a677e0308e5df2abca3
bot <maximBotPy@gmail.com>
2024-08-08 23:27:20 +0300

    вынес валидацию id в отдельный файл

--- back/src/app/chat/schema.py
+++ back/src/app/chat/schema.py
@@ -1,18 +1,7 @@
-from uuid import UUID
-from fastapi import status, HTTPException
-from pydantic import BaseModel, field_validator
-from src.core.validator import ValidUserName
+from pydantic import BaseModel
+from src.core.validator import ValidId
 
 
-class AddUser(BaseModel):
-    user_id: str
-    user_name: ValidUserName
+class SearchUser(BaseModel):
+    user_id: ValidId
 
-    @field_validator("user_id")
-    @classmethod
-    def valid_user_id(cls, id_) -> str:
-        try:
-            UUID(id_)
-            return id_
-        except ValueError:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Некорректный id")
