28d32551c135d848fe0756195fe87f45ad07c350
bot <maximBotPy@gmail.com>
2024-08-08 23:26:06 +0300

    добавил декоратор лимита запросов

--- back/src/core/operations.py
+++ back/src/core/operations.py
@@ -1,18 +1,25 @@
+import time
 import json
 from uuid import uuid4
-from src.config import redis
 from datetime import timedelta
+
+from functools import wraps
+from fastapi import HTTPException, status
+
+from src.config import redis
+from src.config import fast_register
+from src.core.logger import user_logger
 
 
 def generate_uuid() -> str:
-    return str(uuid4())
+    return uuid4().hex
 
 
 def get_seconds(minutes: int) -> int:
     return int(timedelta(minutes=minutes).total_seconds())
 
 
-async def set_redis(name: str, data: dict, ttl: int = 1800): # исправить ttl
+async def set_redis(name: str, data: dict, ttl: int = fast_register.ttl): # исправить ttl
     data_str = json.dumps(data)
     await redis.set(name=name, value=data_str, ex=ttl)
 
@@ -33,3 +40,23 @@ async def save_msg(user_id: str, msg: str, msg_id: int):
     chat = await get_redis(chat_id)
     chat[chat_id].append({"user_id": user_id, "id": msg_id, "msg": msg})
     await set_redis(chat_id, chat)
+
+
+def set_limit_request(time_limit: int, max_calls: int):
+    def decorator(func):
+        calls_list = []
+
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            print(args, kwargs)
+            time_now = time.time()
+            calls_in_time_limit = [call for call in calls_list if call > time_now - time_limit]
+            if len(calls_in_time_limit) >= max_calls:
+                user_logger.warning("Пользователь превысил лимит запросов")
+                raise HTTPException(status_code=status.HTTP_429_TOO_MANY_REQUESTS, detail="Превышен лимит запросов")
+            calls_list.append(time_now)
+            return await func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
