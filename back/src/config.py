875a0afb658b0452ed37aa767ba0f1d9aaf2d41f
bot <maximBotPy@gmail.com>
2024-08-09 23:59:49 +0300

    добавил настройки для быстрой регистрации

--- back/src/config.py
+++ back/src/config.py
@@ -1,3 +1,12 @@
 from redis import asyncio as aioredis
+from pydantic_settings import BaseSettings
 
+
+class FastReg(BaseSettings):
+    min_ttl: int = 300
+    ttl: int = 1800
+    max_ttl: int = 7200
+
+
+fast_register = FastReg()
 redis = aioredis.from_url("redis://localhost", encoding="utf8")
