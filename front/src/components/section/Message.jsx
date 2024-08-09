75cf96f72f8106ce46283b7aebc5047f98ee39fd
bot <maximBotPy@gmail.com>
2024-08-10 00:04:11 +0300

    добавил вывод истории сообщений

--- front/src/components/section/Message.jsx
+++ front/src/components/section/Message.jsx
@@ -1,13 +1,28 @@
 import "./style/message.sass"
 
 
-export function Message() {
+export function Message({ msg }){
+    
     return(
         <section className="section-message">
             <h2 className="hidden__title">
                 Чат
             </h2>
-            <ul id='messages'></ul>
+            <div className="wrapper wrapper-msg">
+                <ul id="msg__container">
+                { msg !== undefined ?
+                    msg.map((m, i) => {
+                        return(
+                        <li className="msg__item" key={ i }>{                   
+                            m["msg"]
+                        }</li>)        
+                    })
+                : <h3 className="msg__none">
+                    Сообщений нет
+                </h3>
+                }
+                </ul>
+            </div>
         </section>
     )
 }
