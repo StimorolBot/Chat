f2bbe5d56bee211c9baa7787e68ccd699da8ae13
bot <maximBotPy@gmail.com>
2024-08-13 00:19:12 +0300

    добавил отображение сообщения при его отправки/получении

--- front/src/components/section/SendMessage.jsx
+++ front/src/components/section/SendMessage.jsx
@@ -1,35 +1,61 @@
 import "./style/send_message.sass"
 
+import { useRef} from "react"
 import { SendMsgBtn } from "../ui/btn/SendMsgBtn"
 import { MsgTextArea } from "../ui/input/MsgTextArea"
+import { useNavigate } from "react-router-dom"
 
 
-export function SendMessage() {
-    const ws = new WebSocket(`http://localhost:8000/1cd57fe5-8878-436a-b85e-cc5ee2eed17f`)
+export function SendMessage({ userIdUrl }) {
+    let msgRef = useRef("")
+    const msgId = Date.now()
+    const navigate = useNavigate()
 
-    ws.onmessage = function(event) {
-        let messages = document.getElementById('messages')
+    const showMsg = (msg) => {
+        let messages = document.getElementById("msg__container")
         let message = document.createElement('li')
-        let content = document.createTextNode(event.data)
+        let content = document.createTextNode(msg)
         message.appendChild(content)
         messages.appendChild(message)
     }
+    
+    const ws = new WebSocket(`http://localhost:8000${userIdUrl}`)
+    
+    ws.onopen = function(e) { console.log("соединение установлено") }
+    ws.onmessage = function(event) { showMsg(event.data) }
+    
+    ws.onerror = function(error) {
+        // Соединение закрыто или не может быть открыто
+        if (error.target.readyState == 3){
+            console.log(error)
+            // navigate("/")
+        }
+    }
 
-    const sendMsg = (event) => {
+    ws.onclose = function(event) {
+        if (event.wasClean) {
+            console.log(`[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
+        } else {
+          // например, сервер убил процесс или сеть недоступна
+          // обычно в этом случае event.code 1006
+          console.log('[close] Соединение прервано');
+        }
+    }
+
+    const sendMsgServer = (event) => {
         event.preventDefault()
-        let inputMsg = document.getElementById("text-msg")
-        ws.send(JSON.stringify({"data": inputMsg.value, "chat_id":"1cd57fe5-8878-436a-b85e-cc5ee2eed17f"}))
-        inputMsg.value = ""
+        ws.send(JSON.stringify({ "data": msgRef.current.value, "id": msgId }))
+        showMsg(msgRef.current.value)
+        msgRef.current.value = ""
     }
 
     return(
         <section className="section-send-message">
             <h2 className="hidden__title"> Отправка сообщений </h2>
-            <form className="form-send-message" action="#" onSubmit={event => sendMsg(event)}>
-                <MsgTextArea id="text-msg"/>
+            <form className="form-send-message" action="#" onSubmit={event => sendMsgServer(event)}>
+                <MsgTextArea innerRef={ msgRef }/>
                 <SendMsgBtn/>
             </form>
         </section>
     )
 }
-
