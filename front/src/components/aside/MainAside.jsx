ae186b4ff184ad5e74db4315d560ac7b6c8b3e2a
bot <maximBotPy@gmail.com>
2024-08-13 00:19:43 +0300

    переименовал пропсы

--- front/src/components/aside/MainAside.jsx
+++ front/src/components/aside/MainAside.jsx
@@ -5,14 +5,14 @@ import { SearchUser } from "../section/SearchUser"
 import { ChatRoom } from "../section/ChatRoom"
 
 
-export function MainAside({ userId, chatList, userInfo, setUserInfo, setUserIdUrl}) {
+export function MainAside({ userId, chatList, setChatList, setUserIdUrl, setMsg}) {
     
     return(
         <aside className="aside">
             <div className="wrapper wrapper-aside">
                 <About userId={ userId } />
-                <SearchUser userInfo={ userInfo } setUserInfo={ setUserInfo }/>    
-                <ChatRoom chatList={ chatList } setUserIdUrl={ setUserIdUrl }/>
+                <SearchUser chatList={ chatList } setChatList={ setChatList }/>    
+                <ChatRoom chatList={ chatList } setUserIdUrl={ setUserIdUrl } setMsg={ setMsg }/>
             </div>
         </aside>
     )
