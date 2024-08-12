8f9a6d83cfbe73da60d4751366965f11c5a3d034
bot <maximBotPy@gmail.com>
2024-08-12 23:14:10 +0300

    добавил логирование

--- back/src/app/chat/connection_manager.py
+++ back/src/app/chat/connection_manager.py
@@ -1,23 +1,28 @@
-from uuid import UUID
 from typing import Dict
 from fastapi import WebSocket
+from src.core.logger import ws_logger
 
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: Dict[UUID:WebSocket] = {}
+        self.active_connections: Dict[str: WebSocket] = {}
 
-    async def connect(self, user_id: UUID, websocket: WebSocket):
+    async def connect(self, user_id: str, websocket: WebSocket):
         await websocket.accept()
-        self.active_connections[user_id] = websocket
+        if not self.active_connections.get(user_id):
+            self.active_connections[user_id] = websocket
+            ws_logger.debug(msg=f"Подключился: {user_id}")
 
-    def disconnect(self, user_id: UUID):
-        del self.active_connections[user_id]
+    def disconnect(self, user_id: str):
+        if self.active_connections.get(user_id):
+            del self.active_connections[user_id]
+            ws_logger.debug(msg=f"Отключился: {user_id}")
 
-    async def send_personal_message(self, message: str, chat_id: UUID):
+    async def send_personal_message(self, message: str, chat_id: str):
         websocket = self.active_connections.get(chat_id)
         if websocket:
             await websocket.send_text(message)
+            ws_logger.debug(f"Сообщение {message} успешно отправлено")
 
     async def broadcast(self, message: str):
         for connection in self.active_connections:
