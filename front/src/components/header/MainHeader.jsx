59c3a605ef04697bccbe17b8a88a373337c9757a
bot <maximBotPy@gmail.com>
2024-08-17 23:49:40 +0300

    внес незначительные изменения

--- front/src/components/header/MainHeader.jsx
+++ front/src/components/header/MainHeader.jsx
@@ -5,9 +5,11 @@ export function MainHeader({ userName }) {
 
     return(
         <header className="header">
-            <h3 className="header__user">
-                { userName }
-            </h3>
+            <div className="wrapper">
+                <h3 className="header__user">
+                    { userName }
+                </h3>
+            </div>
         </header>
     )
 }
