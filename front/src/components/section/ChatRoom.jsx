bf28e18fe6451a645543914023775960d144ee24
bot <maximBotPy@gmail.com>
2024-08-17 23:47:07 +0300

    добавил получение истории сообщений и поменял отображение списка чатов

--- front/src/components/section/ChatRoom.jsx
+++ front/src/components/section/ChatRoom.jsx
@@ -1,33 +1,45 @@
 import "./style/chat_room.sass"
 
+import api from "../../../config/api"
+import { Link } from "react-router-dom"
 
-export function ChatRoom({ chatList, setUserIdUrl }) {
+
+export function ChatRoom({ chatList, setUserIdUrl, setMsg }) {
     
-    const createUrl = async (chatId) => {
-        history.pushState("", "", chatId)
-        setUserIdUrl(chatId)
+    const getMsg = async (event, chatId) => {
+        event.preventDefault()
+        await api.get(`/chat/${chatId}`).then((response) => {
+            console.log(response.data["data"]["msg"])
+            setMsg(response.data["data"]["msg"])
+        }).catch((error) => {
+            console.log(error)
+        })
+        setUserIdUrl(`/${chatId}`)
     }
 
     return(
         <section className="chat-room">
             <h2 className="hidden__title">Чаты</h2>
-            <div className="wrapper wrapper-chat-room">
-                <ul className="chat__container chat__container_none">
-                    { chatList.length != 0 ?
-                        chatList.map((chat, index) => {
-                            return(
-                                <li className="chat__item"
-                                    key={ index } onClick={(event) => createUrl(chat)}>
-                                    { chat }
-                                </li>
-                            )
-                        })
-                    : <h2 className="chat__none">
+            { chatList["id"] !== "" ?
+                <ul className="chat__container">
+                    { chatList.map((chat, index) => {
+                        return(
+                            <li className="chat__item" key={ index }
+                                onClick={(event) => getMsg(event, chat["chat_id"])}>
+                                
+                                <Link className="chat__item-link" to={`/${chat["chat_id"]}`}>
+                                    { chat["chat_name"] }
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+                : <ul className="chat__container_none">
+                    <h2 className="chat__none">
                         Список чатов пуст
                     </h2>
-                    }
                 </ul>
-            </div>
+                }
         </section>
     )
 }
